44945f72720e88c105471ff73d854f419ec47248
Ivan Pavic <ivan.pavic@usi.ch>
2022-01-21 16:51:25 +0100

    fix

--- deploy.js
+++ deploy.js
@@ -1,29 +1,29 @@
 /**
  * Solidity v0.8.11
  */
- const HDWalletProvider = require("@truffle/hdwallet-provider");
- const Web3 = require("web3");
- 
- const { abi, evm } = require("./compile");
- 
- provider = new HDWalletProvider(
-   "know emerge piano inner fun scene summer sustain during sense category roast",
-   "https://rinkeby.infura.io/v3/239f02068b234dc5b70e4430477577c5"
- );
- 
- const web3 = new Web3(provider);
- 
- const deploy = async () => {
-   const accounts = await web3.eth.getAccounts();
- 
-   console.log("Attempting to deploy from account", accounts[0]);
- 
-   const result = await new web3.eth.Contract(abi)
-     .deploy({ data: evm.bytecode.object })
-     .send({ gas: "1000000", from: accounts[0] });
- 
-   console.log("Contract deployed to", result.options.address);
-   provider.engine.stop();
- };
- 
- deploy();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+
+const { abi, evm } = require("./compile");
+
+provider = new HDWalletProvider(
+  "know emerge piano inner fun scene summer sustain during sense category roast",
+  "https://rinkeby.infura.io/v3/239f02068b234dc5b70e4430477577c5"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account: ", accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to: ", result.options.address);
+  provider.engine.stop();
+};
+
+deploy();
